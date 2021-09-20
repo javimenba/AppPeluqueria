ef3e4b6081a5df2c583a0d3745858cf7a4bb668c
Francisco Javier Mendoza Bautista <Pakillo017@gmail.com>
2021-09-20 10:17:12 -0500

    AppSalon

---
+++ src/js/app.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
+});
+
+function iniciarApp() {
+  mostrarServicios();
+}
+
+async function mostrarServicios() {
+  try {
+    const resultado = await fetch("./servicios.json");
+    const db = await resultado.json();
+
+    const { servicios } = db;
+    // Generar el HTML
+    servicios.forEach((servicio) => {
+      const { id, nombre, precio } = servicio;
+
+      // DOM Scripting
+      // Generar nombre de servicio
+      const nombreServicio = document.createElement("P");
+      nombreServicio.textContent = nombre;
+      nombreServicio.classList.add("nombre-servicio");
+
+      // Generar el precio del servicio
+      const precioServicio = document.createElement("P");
+      precioServicio.textContent = `$ ${precio}`;
+      precioServicio.classList.add("precio-servicio");
+
+      // Generar div contenedor de servicio Generar div     contenedor de servicio
+      const servicioDiv = document.createElement("DIV");
+      servicioDiv.classList.add("servicio");
+
+      // Selecciona un sercio para la cita
+      servicioDiv.onclick = seleccionarServicio;
+
+      // Inyectar precio y nombre al div de servicio
+      servicioDiv.appendChild(nombreServicio);
+      servicioDiv.appendChild(precioServicio);
+
+      // Inyectarlo en el HTML
+      document.querySelector("#servicios").appendChild(servicioDiv);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function seleccionarServicio(e) {
+  let elemento;
+  // Forzar que el elemento al cual le damos click sea el DIV
+  if (e.target.tagName === "P") {
+    elemento = e.target.parentElement;
+  } else {
+    elemento = e.target;
+  }
+}
